352f11eba22892cd677c862ecf0f811cb0dbcd6c
ajibadeabd <ajibadeabd@gmail.com>
2020-09-02 06:48:42 +0100

    deployment

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,10 +3,14 @@ let userSevr = require("../services/userServices")
 
 
 class UserContoller {
+
+  // signup 
   async userSignUp(req, res) {
     let {data,status,success,message} = await userSevr.userSignUp(req,res,req.body)
     res.status(status).json({message,...data,status,success});
   }
+  //sign in
+  
   async userSignIn(req, res) {
     let {data,status,success,message} = await userSevr.userSignIn(req,res,req.body)
     res.status(status).json({message,...data,status,success});
